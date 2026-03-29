3dcb4914b5e942473e0e4945db0d1be1bb43fbcb
yunus-I <yunuschatgpt@gmail.com>
2026-03-29 14:23:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,8 +197,8 @@ userBot.on('text', (ctx) => {
     ctx.session.step = 'PAYMENT';
     
     const paymentMsg = ctx.session.lang === 'en'
-      ? `💸 **Payment Required: 200 ETB**\n\n🏦 CBE: 1000651098347 (Yunus Endris)\n📱 Telebirr: 0985711861 (Yunus Endris)\n\n📸 **Send screenshot of payment.**`
-      : `💸 **ክፍያ ያስፈልጋል: 200 ብር**\n\n🏦 ንግድ ባንክ: 1000651098347 (Yunus Endris)\n📱 ቴሌብር: 0985711861 (Yunus Endris)\n\n📸 **የከፈሉበትን ደረሰኝ ስክሪንሾት ይላኩ።**`;
+      ? `💸 **Payment Required: 200 ETB**\n\n🏦 CBE: 1000293836648 (Admassu Yano)\n📱 Telebirr: 0907667755 (Admassu Yano)\n\n📸 **Send screenshot of payment.**`
+      : `💸 **ክፍያ ያስፈልጋል: 200 ብር**\n\n🏦 ንግድ ባንክ: 1000293836648 (Admassu Yano)\n📱 ቴሌብር: 0907667755 (Admassu Yano)\n\n📸 **የከፈሉበትን ደረሰኝ ስክሪንሾት ይላኩ።**`;
     
     ctx.replyWithMarkdown(paymentMsg);
   }
